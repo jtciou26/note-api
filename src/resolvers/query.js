7496e457c11cc3eaf8b42bac4b48a80d231a7c96
jt ciou <jtciou26@gmail.com>
2024-05-28 15:01:51 +0800

    refactor: 💡 resolve conflict

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -54,12 +54,8 @@ module.exports = {
     return await models.Note.find(
       { $text: { $search: keyword }, isRemoved: false },
       { score: { $meta: 'textScore' } }
-<<<<<<< mynotes
     )
       .sort({ score: { $meta: 'textScore' }, updatedAt: -1 })
       .limit(100);
-=======
-    ).sort({ score: { $meta: 'textScore' }, updatedAt: -1 }).limit(100);
->>>>>>> master
   }
 };
